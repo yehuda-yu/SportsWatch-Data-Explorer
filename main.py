f58194ab39f6c19daa76dc9cb389ccb5671e0a9e
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-07-31 20:20:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 from scipy import stats
 import functions
+import plotly.express as px
+import plotly.graph_objs as go
 
 # Define the color palette
 color_palette = ["#21b0fe", "#fed700", "#fe218b"]
