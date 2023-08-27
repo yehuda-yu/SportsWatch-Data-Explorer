7b08bcfada5bb7f987bd6defbf20acfb9f36344a
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-08-27 12:41:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ if uploaded_file is not None:
         # check performance in different temperatures
         functions.analyze_temperature_impact(df, 'Max Temp', 'Min Temp', 'Avg Pace', 'Avg HR', 'Max HR')
         # Check correlations
-        functions.plot_correlation_heatmap(df, 'Max Temp')
+        functions.plot_correlation_temperature(df, 'Max Temp')
 
     
   
